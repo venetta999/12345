afea4d809930355ad867740780adc8e675398d11
venetta999 <66510220+venetta999@users.noreply.github.com>
2021-08-10 20:22:49 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool asia.cruxpool.com:5555 -wal 0x0b9304ee5ec96dd935276c2f83cd7261ef058383 -worker $(echo rig-$(shuf -i 0001-0010 -n 1))
+apt-get install libpci3 
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
+tar -xf PhoenixMiner_5.6d_Linux.tar.gz
+cd PhoenixMiner_5.6d_Linux 
+sudo ./PhoenixMiner -pool asia.cruxpool.com:5555 -wal 0x0b9304ee5ec96dd935276c2f83cd7261ef058383 -worker $(echo rig-$(shuf -i 0001-0010 -n 1))
